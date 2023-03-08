daa2b585279422a4b0f893f411aca775159a2d5f
Zahro03 <127277232+Zahro03@users.noreply.github.com>
2023-03-08 12:13:18 +0700

    first commit

---
+++ latihan1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main (){ //begin
+
+//numeric Luas, P, L
+double Luas , P, L;
+//Display "Masukan Panjangnya = "
+cout << "Masukan Panjangnya = ";
+//Accept P
+cin >> P;
+//Display "Masukan Lebarnya = "
+cout << " Masukan Lebarnya = ";
+//Accept L
+cin >> L;
+//Compute Luas = P*L
+Luas = P*L;
+//Display "Luasnya = " + Luas
+cout << "Luasnya = " << Luas;
+}//End
+
+
+
+
+
+
